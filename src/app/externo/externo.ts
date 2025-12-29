218938056442c2d03a13081b716fa2835d7add8c
sebas0000 <sebastiangomezcalvario@gmail.com>
2025-12-29 12:27:12 -0600

    fin de formulario y uso de este con metodo post

--- src/app/externo/externo.ts
+++ src/app/externo/externo.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 import { PeticionesService } from '../services/peticiones.service';
 import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { response } from 'express';
+import { error } from 'console';
 @Component({
   selector: 'app-externo',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './externo.html',
   styleUrl: './externo.css',
 })
@@ -12,10 +15,18 @@ export class Externo implements OnInit{
 
   public user:any;
   public mostrar:boolean = false;
+  public new_user: any;
+  public usuario_guardado: any
+
 
   constructor(
     private peticionesService : PeticionesService
-  ){}
+  ){
+    this.new_user = {
+      "name": "sebastian",
+      "email":"segok@gmail.com"
+    }
+  }
 
   ngOnInit(){
     this.peticionesService.getUser().subscribe(
@@ -25,6 +36,18 @@ export class Externo implements OnInit{
       },
       error => {
         console.log(error)
+      }
+    )
+  }
+  onSubmit(form:any){
+    this.peticionesService.addUser(this.new_user).subscribe(
+      response => {
+        this.usuario_guardado = response;
+        console.log(response)
+        form.reset();
+      },
+      error => {
+        console.log(<any>error)
       }
     )
   }
--- src/app/services/peticiones.service.ts
+++ src/app/services/peticiones.service.ts
@@ -14,4 +14,10 @@ export class PeticionesService{
     getUser() : Observable<any> {
         return this._http.get(this.url+'users/2');
     }
+    addUser(user: any) : Observable<any> {
+        let params = JSON.stringify(user)
+        let headers = new HttpHeaders().set('Content-Type','application/json');
+
+        return this._http.post(this.url+'users', params, {headers:headers})
+    }
 }
